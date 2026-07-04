3ee8de1dcb6946aa422925181d2b7e3e6fb49d36
ConradPinn <conrad.pinn@gmail.com>
2026-07-04 11:31:40 +0000

    hellow world!

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){ // Hello world main function
+    std::cout << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << std::endl;
+    return 0;
+}
