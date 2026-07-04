17e520698195c61c8df06adcb38c428688f92106
ConradPinn <conrad.pinn@gmail.com>
2026-07-04 21:13:04 +0000

    vectors

--- src/Ch04/04_06b/CodeDemo.cpp
+++ src/Ch04/04_06b/CodeDemo.cpp
@@ -4,8 +4,23 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(){
+    std::vector<std::string> checkpoints = {"start", "forest", "castle"};
+
+    // push back is the equivalent of python .append()
+    checkpoints.push_back("cave");
+    checkpoints.push_back("finish");
+
+    for (int i; i < checkpoints.size(); i++) {
+        // we can change individual indexes in the vector
+        if (i == 2) {
+            checkpoints[i] = "dark castle";
+        }
+        
+        std::cout << checkpoints[i] << std::endl;
+    }
     
     std::cout << std::endl << std::endl;
     return 0;
