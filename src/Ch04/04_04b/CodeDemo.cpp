1c0ec480117a18c1a09b04daadb53f334bb9b728
ConradPinn <conrad.pinn@gmail.com>
2026-07-04 20:50:05 +0000

    allocating and deallocating memory using pointers

--- src/Ch04/04_04b/CodeDemo.cpp
+++ src/Ch04/04_04b/CodeDemo.cpp
@@ -4,7 +4,7 @@
 
 #include <iostream>
 #include <string>
-#include <chrono>
+#include <chrono> // classes to keep track of time
 
 // Type aliases for chrono classes
 using Clock = std::chrono::system_clock;
@@ -12,19 +12,26 @@ using TimePoint = std::chrono::time_point<Clock>;
 using Duration = std::chrono::duration<double>;
 
 int main(){
-    TimePoint start, end;
-    Duration elapsed_seconds;
+    TimePoint *start, *end; // asterisks mean that these are now pointers
+    Duration *elapsed_seconds = new Duration(); // elapsed second is a pointer hence new key word is required to allocate memory for the sotred Duration, new allocates memory in cpp
     std::string input;
 
-    start = Clock::now();
+    start = new TimePoint(Clock::now()); // same as above, new is required to allocate memory to start
 
     std::cout << "Type 'start' as quickly as you can and hit ENTER: " << std::flush;
     std::cin >> input;
 
-    end = Clock::now();
-    elapsed_seconds = end - start;
+    end = new TimePoint(Clock::now());
+    *elapsed_seconds = *end - *start; // all variables here are pointers
 
-    std::cout << "Reaction time: " << elapsed_seconds.count() << "s" << std::endl;
+    // there are 2 different ways of accessing the count of the pointer
+    std::cout << "Reaction time: " << (*elapsed_seconds).count() << "s" << std::endl;
+    std::cout << "Reaction time: " << elapsed_seconds->count() << "s" << std::endl;
+
+    // we need to delete these now as we are dynamically assigning memory on the fly and delete deallocates memory in cpp
+    delete start;
+    delete end;
+    delete elapsed_seconds;
     
     std::cout << std::endl << std::endl;
     return 0;
