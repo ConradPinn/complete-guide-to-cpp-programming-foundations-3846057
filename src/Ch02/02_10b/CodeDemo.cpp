f4122405839bea46cf496589db707a3ec9a86e7b
ConradPinn <conrad.pinn@gmail.com>
2026-07-04 13:56:24 +0000

    type casting

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,24 @@
 #include <cstdint>
 
 int main(){
-    
+    float target_x;
+    int32_t sprite_x;
+    uint32_t player_x;
+
+    target_x = -123.45; // is a float
+    sprite_x = target_x; // cast to a signed int
+    player_x = sprite_x; // cast to an unsigned int
+
+    // the point to note here is that all of these values have the same
+    // binary representation, but depending on their type, determines how
+    // this is actually interpreted, you will note the 3rd print statement
+    // leads to an overflow error, where as the 4th does not because we 
+    // cast specifically to a signed int rather than unsigned
+    std::cout << target_x << std::endl;
+    std::cout << sprite_x << std::endl;
+    std::cout << player_x << std::endl;
+    std::cout << static_cast<int32_t>(player_x) << std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
