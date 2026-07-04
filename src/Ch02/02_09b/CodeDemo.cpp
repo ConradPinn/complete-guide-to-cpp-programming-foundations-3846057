97c583f092fc7bfccf86fc29bad9f8955893ad03
ConradPinn <conrad.pinn@gmail.com>
2026-07-04 13:32:25 +0000

    struct

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -7,7 +7,22 @@
 
 enum class character_role {protagonist, antagonist, sidekick, npc};
 
+struct game_character {
+    std::string name;
+    int level;
+    character_role role;
+};
+
 int main(){
+
+    game_character tony;
+    tony.name = "Tony";
+    tony.level = 10;
+    tony.role = character_role::sidekick;
+
+    std::cout << tony.name << std::endl;
+    std::cout << tony.level << std::endl;
+    std::cout << (int) tony.role << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
