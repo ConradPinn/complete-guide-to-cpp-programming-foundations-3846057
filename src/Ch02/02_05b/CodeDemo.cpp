f1c307b80ac5534c6a2c0c1c0fd64b41231dd676
ConradPinn <conrad.pinn@gmail.com>
2026-07-04 12:59:27 +0000

    data types

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,28 @@
 
 #include <iostream>
 
+// global variables
+int a;
+int b = 5;
+
 int main(){
-    
+
+    bool my_flag;
+    my_flag = false;
+
+    // note that we do not need to declare the type of a
+    // because this was already done in the global scope
+    a = 7;
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "my_flag = " << my_flag << std::endl;
+
+    my_flag = true;
+
+    std::cout << "my_flag = " << my_flag << std::endl;
+    std::cout << "b + a = " << b + a << std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
