46d4b54a97b99a11f9fdd1dff7d57b643041490d
ConradPinn <conrad.pinn@gmail.com>
2026-07-04 18:30:31 +0000

    arrays and loops

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -5,7 +5,21 @@
 #include <iostream>
 
 int main(){
+    const int number_of_enemies = 4;
+
+    int enemies[number_of_enemies];
+    enemies[0] = 10;
+    enemies[1] = 15;
+    enemies[2] = 20;
+    enemies[3] = 25;
+
+    float difficulty[] = {1.0, 2.1, 3.2, 4.7};
+
+    for (int i = 0; i < number_of_enemies; i++) {
+        std::cout << enemies[i] << std::endl;
+        std::cout << difficulty[i] << std::endl;
+        std::cout << std::endl << std::endl;
+    }
     
-    std::cout << std::endl << std::endl;
     return 0;
 }
