4fd7d963e691a694d8b04e240f2f531b27cbd42a
ConradPinn <conrad.pinn@gmail.com>
2026-07-05 08:06:51 +0000

    if statements

--- src/Ch05/05_01b/CodeDemo.cpp
+++ src/Ch05/05_01b/CodeDemo.cpp
@@ -3,11 +3,32 @@
 // If Statements, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 int main(){
     int score = 1023;
     bool gamePaused = false;
     char action = 'x';
+
+    if (score > 1000) {
+        std::cout << "score is over 1000" << std::endl;
+    }
+
+    if (score % 2 == 0) {
+        std::cout << "score is even" << std::endl;
+    } else {
+        std::cout << "score is odd" << std::endl;
+    }
+
+    std::vector<char> valid_moves = {'w','a','s','d','W','A','S','D',};
+
+    if(std::find(valid_moves.begin(), valid_moves.end(), action) != valid_moves.end()) {
+        std::cout << "valid move" << std::endl;
+    } else {
+        std::cout << "invalid move" << std::endl;
+    }
     
     std::cout << std::endl << std::endl;
     return 0;
