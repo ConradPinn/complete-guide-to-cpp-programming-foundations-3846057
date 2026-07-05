475f62556270fbdc2b93fe4d341a0b8d2053a83e
ConradPinn <conrad.pinn@gmail.com>
2026-07-05 11:07:09 +0000

    defining and calling functions

--- src/Ch06/06_04b/CodeDemo.cpp
+++ src/Ch06/06_04b/CodeDemo.cpp
@@ -5,11 +5,25 @@
 #include <iostream>
 #include <string>
 
-int main(){
-    std::string playerName = "Alex";
-    int score = 75;
+std::string getPlayerName() {
+    return "Alex";
+}
 
+int calculateScore(int baseScore, int multiplier) {
+    return baseScore * multiplier;
+}
+
+// const is useful here as it tells us the function will not mutate either value
+// references (&) are useful here as the function does not have to copy either variable when called
+void displayScore(const std::string& playerName, const int& score) {
     std::cout << playerName << " scored " << score << " points." << std::endl;
+}
+
+int main(){
+    std::string playerName = getPlayerName();
+    int score = calculateScore(75, 2);
+
+    displayScore(playerName, score);
     
     std::cout << std::endl << std::endl;
     return 0;
